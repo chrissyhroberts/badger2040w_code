169ebf08327929ccb0b71f0475b44b92544f2625
chrissyhroberts <chrissyhroberts@yahoo.co.uk>
2023-07-26 10:49:47 +0100

    RTC functions
    Adds a function that sets all three clocks from ntptime
    Adds a function that calls back all three clocks to show which are maintained on battery when machine halts.
    In general you should use PCF85063A RTC if you want time to keep ticking whilst sleeping/halted

---
+++ Clock_Stuff/cl1.py
@@ -0,0 +1,65 @@
+import machine
+import badger2040
+import time
+import utime
+import ntptime
+from pcf85063a import PCF85063A
+import badger_os
+
+badger = badger2040.Badger2040()
+
+badger.set_pen(15)
+badger.clear()
+badger.set_pen(1)
+
+badger.connect()
+
+if badger.isconnected():
+    # Synchronize with the NTP server to get the current time
+    ntptime.settime()
+
+# Get the time after synchronizing with the NTP server
+ut = str(machine.RTC().datetime())
+
+
+badger.set_pen(15)
+badger.clear()
+badger.set_pen(1)
+badger.text(f"ut: {ut}", 10, 0, 1)
+badger.update()
+time.sleep(0.05)
+
+print(utime.localtime())
+# Set the time on the Pico's onboard RTC
+def set_pico_time():
+    rtc = machine.RTC()
+    now = utime.localtime()
+    rtc.datetime((now[0], now[1], now[2], now[6], now[3], now[4], now[5], 0))
+
+# Set the time on the external PCF85063A RTC
+def set_pcf85063a_time():
+    now = utime.localtime()
+    i2c = machine.I2C(0, scl=machine.Pin(5), sda=machine.Pin(4))
+    rtc_pcf85063a = PCF85063A(i2c)
+    rtc_pcf85063a.datetime((now[0], now[1], now[2], now[3], now[4], now[5], now[6]))
+
+# Set the time on the Pico's onboard RTC
+set_pico_time()
+
+# Set the time on the external PCF85063A RTC
+set_pcf85063a_time()
+
+# Get the time after setting the RTCs
+ut2 = str(machine.RTC().datetime())
+
+
+badger.text(f"Pico_RTC: {ut}", 80, 0, 1)
+badger.text(f"PCF_RTC: {ut2}", 200, 0, 1)
+badger.update()
+time.sleep(0.05)
+
+print("Pico RTC:", ut)
+print("PCF85063A RTC:", ut2)
+
+badger_os.launch("launcher")
+
