8907854c4ff153dd18ac3f61e383d88c0a613846
chrissyhroberts <chrissyhroberts@yahoo.co.uk>
2023-08-02 17:29:50 +0100

    Update power.py
    Fixes problem with battery monitor.

--- power_and_disk/power.py
+++ power_and_disk/power.py
@@ -2,6 +2,8 @@ import machine
 import badger2040
 import badger_os #https://github.com/pimoroni/badger2040/blob/main/firmware/PIMORONI_BADGER2040/lib/badger_os.py
 import utime
+import network
+from machine import ADC, Pin
 
 #####################################
 # Define functions
@@ -26,6 +28,62 @@ def cls():
     display.clear()
     display.set_pen(1)
     display.update()
+    
+def get_battery_info(full_battery=3.7, empty_battery=2.8):
+    # Pico W voltage read function by darconeous on reddit: 
+    # https://www.reddit.com/r/raspberrypipico/comments/xalach/comment/ipigfzu/
+
+    # Initialize Variables
+    conversion_factor = 3 * full_battery / 65535
+    voltage=0
+    percentage=0
+    is_charge=False
+    error_message=None
+    
+    # prep the network
+    wlan = network.WLAN(network.STA_IF)
+    wlan_active = wlan.active()
+
+    try:
+        # Don't use the WLAN chip for a moment.
+        wlan.active(False)
+
+        # Make sure pin 25 is high.
+        Pin(25, mode=Pin.OUT, pull=Pin.PULL_DOWN).high()
+        
+        # Reconfigure pin 29 as an input.
+        Pin(29, Pin.IN)
+        
+        vsys = ADC(29)
+        
+        # get the voltage
+        voltage=vsys.read_u16() * conversion_factor
+        
+        # figure out the percentage of available battery
+        if voltage:
+            try:
+                percentage = 100 * ((voltage - empty_battery) / (full_battery - empty_battery))
+            except:
+                percentage = 0
+                
+            if percentage > 100:
+                percentage = 100.00
+            elif percentage < 0:
+                percentage = 0
+                
+            charging = Pin('WL_GPIO2', Pin.IN)  # reading this pin tells us whether or not USB power is connected
+            is_charge = charging.value()
+        
+    except Exception as e:
+        error_message=str(e)
+
+    finally:
+        # Restore the pin state and possibly reactivate WLAN
+        Pin(29, Pin.ALT, pull=Pin.PULL_DOWN, alt=7)
+        wlan.active(wlan_active)
+    
+    return {"error":error_message, "voltage":voltage, "percentage":percentage, "full_battery":full_battery, "empty_battery":empty_battery, "is_charge":is_charge}
+
 
 #####################################
 # Find values
@@ -49,35 +107,44 @@ display = badger2040.Badger2040()
 WIDTH = badger2040.WIDTH # 296
 HEIGHT = badger2040.HEIGHT # 128
 display.rectangle(0, 60, WIDTH, 25)
-display.set_font("bitmap8")
 
-batlevel = badger_os.get_battery_level()
+batlevel = get_battery_info()
 print(f"battery: {batlevel}%")
 diskusage = badger_os.get_disk_usage()
 print(f"diskusage: {diskusage}%")
 staterunning = badger_os.state_running()
 print(f"staterunning: {staterunning}%")
-
+print(get_battery_info())
+print(f"Battery 2 :  {get_battery_info()}")
+print(batlevel['voltage'])
 
 cls()
-# Update all the text values using the stored voltage values
-display.text(f"Voltage", 10, 20,WIDTH,2)
-display.text(f"| 26: {pin26} V | 27: {pin27} V", 85, 10,WIDTH,2)
-display.text(f"| 28: {pin28} V | 29: {pin28} V", 85, 30,WIDTH,2)
+# Draw a grid
+display.line(85, 0, 85, 300, 3)
+display.line(0, 40, 300, 40, 3)
+display.line(0, 70, 300, 70, 3)
 
-display.text(f"Battery",10, 50,WIDTH,2)
-display.text(f"| {batlevel}%",85, 50,WIDTH,2)
+# Add voltage across various pins
+display.text(f"Voltage ", 0, 15,WIDTH,2)
+display.text(f"| 26: {pin26} V | 27: {pin27} V", 85, 5,WIDTH,2)
+display.text(f"| 28: {pin28} V | 29: {pin28} V", 85, 20,WIDTH,2)
+
+# Show the battery state
+display.text(f"Battery ",0, 50,WIDTH,2)
+display.text(f"| {round(batlevel['voltage'],2)}V | {round(batlevel['percentage'],2)}%",85, 50,WIDTH,2)
     
-    
-display.text(f"Disk", 10, 90,WIDTH,2)
-display.text(f"| Total : {round(diskusage[0],2)}", 85, 70,WIDTH,2)
-display.text(f"| Used : {round(diskusage[1],2)}", 85, 90,WIDTH,2)
-display.text(f"| Free : {round(diskusage[2],2)}", 85, 110,WIDTH,2)
+# Show the disk space used/available    
+display.text(f"Disk", 0, 90,WIDTH,2)
+display.text(f"| Total : {round(diskusage[0]/10000,2)}", 85, 75,WIDTH,2)
+display.text(f"| Used   : {round(diskusage[1],2)}", 85, 90,WIDTH,2)
+display.text(f"| Free   : {round(diskusage[2],2)}", 85, 105,WIDTH,2)
     
 display.update()
-utime.sleep_ms(2000)
-badger_os.launch('launcher')
+#utime.sleep_ms(2000)
+#badger_os.launch('launcher')
 
 # Add a short delay (e.g., 500 milliseconds)
 # Update the display
+
+
 
