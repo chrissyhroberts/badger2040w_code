4ae5ce7c6382a33586e720fe312d63c3cca0f44d
chrissyhroberts <chrissyhroberts@yahoo.co.uk>
2023-08-22 14:25:59 +0100

    Update logger.py
    adds humidity measurements

--- aht20/logger.py
+++ aht20/logger.py
@@ -19,6 +19,7 @@ def clear():
     display.set_font("bitmap8")
     display.set_pen(0)
     display.rectangle(0, 0, WIDTH, 10)
+    display.rectangle(0, HEIGHT-10, WIDTH, HEIGHT-10)
     display.set_pen(15)
     display.text("Badger App provisioning", 10, 1, WIDTH, 0.6)
     display.set_pen(0)
@@ -31,11 +32,6 @@ def get_iso_timestamp():
     return iso_timestamp
 
 
-# Initialize I2C
-i2c = I2C(id=0, scl=Pin(5), sda=Pin(4))
-
-# Create the sensor object using I2C
-sensor = ahtx0.AHT20(i2c)
 
 # Define the chart area
 chart_width = 200
@@ -62,24 +58,35 @@ csv_file_path = "data/logged_data.csv"
 
 
 temperature_values = []
+humidity_values = []
+
 y_scale = 40  # Initial y-axis scale (0 to 80)
 measurement_count = 0
 
 try:
     while True:
+        
+        # Initialize I2C
+        i2c = I2C(id=0, scl=Pin(5), sda=Pin(4))
+
+        # Create the sensor object using I2C
+        sensor = ahtx0.AHT20(i2c)
         # Read temperature from the sensor
        # Read temperature from the sensor
         temperature = sensor.temperature
+        humidity = sensor.relative_humidity
 
         # Get the current timestamp in ISO format
         timestamp = get_iso_timestamp()
 
         # Append the measurement to the CSV file
         with open(csv_file_path, "a") as csv_file:
-            csv_file.write("{}, {:.2f}\n".format(timestamp, temperature))
+            csv_file.write("{}, {:.2f}, {:.2f}\n".format(timestamp, temperature, humidity))
 
-        # Store the temperature value
+        # Store the temperature and humidity value
         temperature_values.append(temperature)
+        humidity_values.append(humidity)
+
         measurement_count += 1
 
         # Check button UP state
@@ -100,8 +107,8 @@ try:
 
         # Draw the axes
         display.set_pen(0)
-        display.line(chart_origin_x, chart_origin_y + chart_height, chart_origin_x + chart_width, chart_origin_y + chart_height)  # X-axis
-        display.line(chart_origin_x, chart_origin_y, chart_origin_x, chart_origin_y + chart_height)  # Y-axis
+        display.line(chart_origin_x, chart_origin_y + chart_height, chart_origin_x + chart_width, chart_origin_y + chart_height,2)  # X-axis
+        display.line(chart_origin_x, chart_origin_y, chart_origin_x, chart_origin_y + chart_height,2)  # Y-axis
 
         # Draw tickmarks and reference values on the y-axis
         for i in range(0, y_scale + 1, 10):
@@ -118,20 +125,34 @@ try:
         for i in range(1, len(temperature_values)):
             x1 = chart_origin_x + int((i - 1) * chart_width / (len(temperature_values) - 1))
             x2 = chart_origin_x + int(i * chart_width / (len(temperature_values) - 1))
-            
+       # Draw the humidity data as lines
+        for i in range(1, len(humidity_values)):
+            x1 = chart_origin_x + int((i - 1) * chart_width / (len(humidity_values) - 1))
+            x2 = chart_origin_x + int(i * chart_width / (len(humidity_values) - 1))
+    
+            y1_hum = chart_origin_y + chart_height - int((humidity_values[i - 1] * (chart_height / y_scale)))
+            y2_hum = chart_origin_y + chart_height - int((humidity_values[i] * (chart_height / y_scale)))
+    
+            display.set_pen(7)  # Using blue for humidity
+            display.line(x1, y1_hum, x2, y2_hum, 2)
+     
             # Calculate the y-coordinate for the trace line
             y1 = chart_origin_y + chart_height - int((temperature_values[i - 1] * (chart_height / y_scale)))
             y2 = chart_origin_y + chart_height - int((temperature_values[i] * (chart_height / y_scale)))
             
             display.set_pen(0)
-            display.line(x1, y1, x2, y2)
+            display.line(x1, y1, x2, y2,2)
 
         # Calculate and display the average temperature
         if temperature_values:
             average_temp = sum(temperature_values) / len(temperature_values)
+            average_humidity = sum(humidity_values) / len(humidity_values)
+
             display.set_pen(15)  # Pen color 0 is black
-            display.text("Avg: {:.2f}°C".format(average_temp), 240, 1, WIDTH, 0.6)
+            display.text("Avg: {:.2f}°C".format(average_temp), 150, HEIGHT-9, WIDTH, 0.6)
             display.text("Current: {:.2f}°C".format(temperature), 150, 1, WIDTH, 0.6)  # Print current temperature
+            display.text("| Avg: {:.2f}%RH".format(average_humidity), 240, HEIGHT-9, WIDTH, 0.6)
+            display.text("Current: {:.2f}%RH".format(humidity), 240, 1, WIDTH, 0.6)
             print(f"Average temperature: {average_temp} | Current temperature: {temperature}")
 
         display.update()
@@ -142,7 +163,7 @@ try:
             measurement_count = 0
 
         # Sleep for a while before the next observation
-        utime.sleep(1)  # Adjust the sleep duration as needed
+        utime.sleep(10)  # Adjust the sleep duration as needed
 
 except KeyboardInterrupt:
     pass
