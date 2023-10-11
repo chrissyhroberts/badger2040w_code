242e6158e82ede5fc045b832b6a321a3d0e8e8c2
chrissyhroberts <chrissyhroberts@yahoo.co.uk>
2023-10-11 11:15:45 +0100

    Update totp.py
    pads single digits on date and time to be 2 digits between 0 and 9

--- examples/totp.py
+++ examples/totp.py
@@ -238,10 +238,19 @@ for info in key_info:
 
 #show current date and time
 spot_time = machine.RTC().datetime()
+year = spot_time[0]
+month = spot_time[1]
+day = spot_time[2]
 hour = spot_time[4]
+minute = spot_time[5]
 hour = hour + timezone_offset
-badger.text(f"{spot_time[0]}-{spot_time[1]}-{spot_time[2]}", 200, 70, WIDTH, 2)
-badger.text(f"{hour}:{spot_time[5]}", 200, 90, WIDTH, 3)
+
+month = ('00' + str(month))[-2:]
+day = ('00' + str(day))[-2:]
+hour = ('00' + str(hour))[-2:]
+minute = ('00' + str(minute))[-2:]
+badger.text(f"{year}-{month}-{day}", 200, 70, WIDTH, 2)
+badger.text(f"{hour}:{minute}", 200, 90, WIDTH, 3)
 badger.update()
 
 while True:
@@ -286,11 +295,21 @@ while True:
                 x += 100  # Add 80 to x
         #show current date and time
         #show current date and time
+        #show current date and time
         spot_time = machine.RTC().datetime()
+        year = spot_time[0]
+        month = spot_time[1]
+        day = spot_time[2]
         hour = spot_time[4]
+        minute = spot_time[5]
         hour = hour + timezone_offset
-        badger.text(f"{spot_time[0]}-{spot_time[1]}-{spot_time[2]}", 200, 70, WIDTH, 2)
-        badger.text(f"{hour}:{spot_time[5]}", 200, 90, WIDTH, 3)
+
+        month = ('00' + str(month))[-2:]
+        day = ('00' + str(day))[-2:]
+        hour = ('00' + str(hour))[-2:]
+        minute = ('00' + str(minute))[-2:]
+        badger.text(f"{year}-{month}-{day}", 200, 70, WIDTH, 2)
+        badger.text(f"{hour}:{minute}", 200, 90, WIDTH, 3)
         badger.update()
         utime.sleep_ms(25000)
         null, cadence = otp_value, remaining = totp(get_pcf_time(), "LMESUJEY7PTJSNYO5LKSME5HWQO6XZ5L", 30, 6)
